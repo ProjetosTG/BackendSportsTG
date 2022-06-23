553b15e19c4491d9de2ed4e68cea86313fc8b41b
GuiStefanin <gui.stefanin@gmail.com>
2022-06-23 18:57:33 -0300

    primeiro commit

---
+++ src/main/java/com/projetostg/sportstg/SportstgApplication.java
@@ -0,0 +1,13 @@
+package com.projetostg.sportstg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportstgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportstgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetostg/sportstg/SportstgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetostg.sportstg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportstgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
